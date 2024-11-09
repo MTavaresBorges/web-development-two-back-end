408091d414148b7875f5953c649c140ea84dc334
MTavareBorges <matheustavaresborges2000@gmail.com>
2024-11-08 21:59:16 -0300

    feat: migrations have been inserted

---
+++ prisma/migrations/20241109003921_add_user_products_relation/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `full_name` VARCHAR(250) NOT NULL,
+    `email` VARCHAR(200) NOT NULL,
+    `password` VARCHAR(256) NOT NULL,
+
+    UNIQUE INDEX `email_UNIQUE`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(200) NOT NULL,
+    `description` VARCHAR(800) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `banner` VARCHAR(300) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
