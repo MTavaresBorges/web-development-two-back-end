7ed2a78594dd9bba012123c7aa24c58b769e01b9
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-11-13 22:37:12 -0300

    feat: CORS installation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRouter from './routers/userRouter.js';
 
 dotenv.config();
 
 const app = express();
+
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Origin not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+// app.use(cors(corsOptions));
+
+// app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
